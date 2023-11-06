a64c176a45233f9209cf70bb19829cbc7c119d4d
Tan Zien <nabsdh9@gmail.com>
2023-11-07 03:09:28 +0800

    rockchip: NanoPi R4S 1GB DDR3
    u-boot: v2021.07

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -45,7 +45,7 @@ TARGET_DEVICES += friendlyarm_nanopi-r2s
 define Device/friendlyarm_nanopi-r4s
   DEVICE_VENDOR := FriendlyARM
   DEVICE_MODEL := NanoPi R4S
-  DEVICE_VARIANT := 4GB LPDDR4
+  DEVICE_VARIANT := 1GB DDR3
   SOC := rk3399
   IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
   DEVICE_PACKAGES := kmod-r8169
